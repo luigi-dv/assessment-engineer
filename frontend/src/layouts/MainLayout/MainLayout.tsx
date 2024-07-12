844228cdc9d36f23fa2ede0c1db2e5446ebcbb0e
Luigi Davila <luigelo@ldvloper.com>
2024-07-12 09:50:17 +0200

    Add Simple Navigation :)

--- frontend/src/layouts/MainLayout/MainLayout.tsx
+++ frontend/src/layouts/MainLayout/MainLayout.tsx
@@ -1,18 +1,12 @@
-import { Link, Outlet } from "react-router-dom";
-import { HOME } from "src/routes/path";
+import { Outlet } from "react-router-dom";
+import { Navigation } from "src/components/Navigation";
 
 export const MainLayout = () => {
   return (
     <div>
       {/* A "layout route" is a good place to put markup you want to
           share across all the pages on your site, like navigation. */}
-      <nav>
-        <ul>
-          <li>
-            <Link to={HOME}>Home</Link>
-          </li>
-        </ul>
-      </nav>
+      <Navigation />
 
       <hr />
       <Outlet />
